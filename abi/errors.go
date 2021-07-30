b76e4f1a87b7041734a442de1e55bdfa75e36b11
yayanyang <org.lemon@gmail.com>
2021-07-30 14:14:16 +0800

    update

--- abi/errors.go
+++ abi/errors.go
@@ -10,4 +10,5 @@ var (
 	ErrBits       = errors.New("integer type bits out of range or is not a multiple of 8", errors.WithVendor(errVendor), errors.WithCode(-1))
 	ErrValue      = errors.New("encode value type error", errors.WithVendor(errVendor), errors.WithCode(-2))
 	ErrFixedBytes = errors.New("fixed bytes length mismatch", errors.WithVendor(errVendor), errors.WithCode(-3))
+	ErrLength     = errors.New("length error", errors.WithVendor(errVendor), errors.WithCode(-4))
 )
